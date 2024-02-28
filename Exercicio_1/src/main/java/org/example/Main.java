2df82ae31824dbdacf06ed7fd8d2087743db7e8c
Ana Luisa Galvao Valeta <analuisagvaleta@gmail.com>
2024-02-28 09:53:37 -0300

    Commit

---
+++ Exercicio_1/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int Lanche1 = 10;
+        int Lanche2 = 4;
+        int Lanche3 = 2;
+
+        int resultado = (Lanche1 + Lanche2 + Lanche3);
+        int media = (resultado)/2;
+
+        System.out.println("O total de lanches consumidos foram de: " +resultado);
+        System.out.println("A media de lanches foi de: " +media);
+    }
+}
